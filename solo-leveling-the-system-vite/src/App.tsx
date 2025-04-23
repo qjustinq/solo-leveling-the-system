f764bd6951f72083958f68ae7ecf83e8abdb627f
qjustinq <justinchen0987@gmail.com>
2025-04-23 16:41:52 -0400

    Adding packages to repo
    Adding packages to repo

---
+++ solo-leveling-the-system-vite/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600 text-white text-4xl font-bold">
+      Testing
+    </div>
+  );
+}
